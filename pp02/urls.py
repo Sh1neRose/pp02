7e5a83e3498afb312a1914c81433120258f45306
Maksym <maxim03102007@gmail.com>
2025-09-26 20:30:07 +0200

    added main file urls for books

--- pp02/urls.py
+++ pp02/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mailing.urls', namespace='mailing')),
-    path('', include('news.urls', namespace='news')),
+    path('', include('books.urls', namespace='books')),
 ]
