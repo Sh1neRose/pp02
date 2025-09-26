12ff0c4b7029d661b18df80c4320ef2aa3e23537
Maksym <maxim03102007@gmail.com>
2025-09-26 20:29:47 +0200

    added app books in settings

--- pp02/settings.py
+++ pp02/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'news.apps.NewsConfig',
+    'books.apps.BooksConfig',
     'mailing.apps.MailingConfig',
 ]
 
